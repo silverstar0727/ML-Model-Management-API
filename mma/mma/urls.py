a5fee74bd183124c1c8545c89ff63e668fe5df8c
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-12-21 12:57:26 +0900

    add swagger

--- mma/mma/urls.py
+++ mma/mma/urls.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+
+schema_url_patterns = [
+    path('api/', include('api.urls')),
+    ]
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="Open API",
+        default_version='v1',
+        description="시스템 API",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    patterns=schema_url_patterns,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include('api.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+         schema_view_v1.without_ui(cache_timeout=0),
+         name='schema-json'
+         ),
+    re_path(r'^swagger/$',
+         schema_view_v1.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'
+         ),
+    re_path(r'^redoc/$',
+         schema_view_v1.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'
+         ),
 ]
